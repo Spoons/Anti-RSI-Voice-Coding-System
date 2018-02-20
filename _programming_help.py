271694ccdf9fbc04fdac2ed354e31687b9212b8f
miffi <mciocio1@uncc.edu>
2018-02-20 00:09:11 -0500

    Created a folder for rules and moved SeriesMappingRule it.

--- _programming_help.py
+++ _programming_help.py
@@ -14,23 +14,7 @@ from lib.dynamic_aenea import (
     Text,
 )
 
-class SeriesMappingRule(CompoundRule):
-
-    def __init__(self, mapping, extras=None, defaults=None):
-        mapping_rule = MappingRule(mapping=mapping, extras=extras,
-                                   defaults=defaults, exported=False)
-        single = RuleRef(rule=mapping_rule)
-        series = Repetition(single, min=1, max=16, name="series")
-
-        compound_spec = "<series>"
-        compound_extras = [series]
-        CompoundRule.__init__(self, spec=compound_spec,
-                              extras=compound_extras, exported=True)
-
-    def _process_recognition(self, node, extras):  # @UnusedVariable
-        series = extras["series"]
-        for action in series:
-            action.execute()
+from rules.series_mapping import SeriesMappingRule
 
 series_rule = SeriesMappingRule(
     mapping={
--- dynamics/javascript_grammar.py
+++ dynamics/javascript_grammar.py
@@ -15,6 +15,8 @@ from lib.dynamic_aenea import (
     Text,
 )
 
+from rules.series_mapping import SeriesMappingRule
+
 import lib.format
 from lib.text import SCText
 
@@ -34,25 +36,6 @@ def define_function(text):
     Text("() {").execute()
     Key("left:3").execute()
 
-
-
-class SeriesMappingRule(CompoundRule):
-
-    def __init__(self, mapping, extras=None, defaults=None):
-        mapping_rule = MappingRule(mapping=mapping, extras=extras,
-                                   defaults=defaults, exported=False)
-        single = RuleRef(rule=mapping_rule)
-        series = Repetition(single, min=1, max=16, name="series")
-
-        compound_spec = "<series>"
-        compound_extras = [series]
-        CompoundRule.__init__(self, spec=compound_spec,
-                              extras=compound_extras, exported=True)
-
-    def _process_recognition(self, node, extras):  # @UnusedVariable
-        series = extras["series"]
-        for action in series:
-            action.execute()
 
 rules = SeriesMappingRule(
     mapping={
