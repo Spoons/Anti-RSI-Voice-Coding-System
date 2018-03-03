c4e6cb32a3228bb45eea8ccc79c4dffa57d8fc9e
miffi <michaelciociola123@gmail.com>
2018-03-03 16:28:15 -0500

    removed comments

--- multiedit/multiedit.py
+++ multiedit/multiedit.py
@@ -3,65 +3,6 @@
 # Licensed under the LGPL, see <http://www.gnu.org/licenses/>
 #
 
-"""
-Command-module for cursor movement and **editing**
-============================================================================
-
-This module allows the user to control the cursor and 
-efficiently perform multiple text editing actions within a 
-single phrase.
-
-
-Example commands
-----------------------------------------------------------------------------
-
-*Note the "/" characters in the examples below are simply 
-to help the reader see the different parts of each voice 
-command.  They are not present in the actual command and 
-should not be spoken.*
-
-Example: **"up 4 / down 1 page / home / space 2"**
-   This command will move the cursor up 4 lines, down 1 page,
-   move to the beginning of the line, and then insert 2 spaces.
-
-Example: **"left 7 words / backspace 3 / insert hello Cap world"**
-   This command will move the cursor left 7 words, then delete
-   the 3 characters before the cursor, and finally insert
-   the text "hello World".
-
-Example: **"home / space 4 / down / 43 times"**
-   This command will insert 4 spaces at the beginning of 
-   of this and the next 42 lines.  The final "43 times" 
-   repeats everything in front of it that many times.
-
-
-Discussion of this module
-----------------------------------------------------------------------------
-
-This command-module creates a powerful voice command for 
-editing and cursor movement.  This command's structure can 
-be represented by the following simplified language model:
-
- - *CommandRule* -- top-level rule which the user can say
-    - *repetition* -- sequence of actions (name = "sequence")
-       - *KeystrokeRule* -- rule that maps a single 
-         spoken-form to an action
-    - *optional* -- optional specification of repeat count
-       - *integer* -- repeat count (name = "n")
-       - *literal* -- "times"
-
-The top-level command rule has a callback method which is 
-called when this voice command is recognized.  The logic 
-within this callback is very simple:
-
-1. Retrieve the sequence of actions from the element with 
-   the name "sequence".
-2. Retrieve the repeat count from the element with the name
-   "n".
-3. Execute the actions the specified number of times.
-
-"""
-
 try:
     import pkg_resources
     pkg_resources.require("dragonfly >= 0.6.5beta1.dev-r99")
@@ -115,22 +56,6 @@ else:
     FormatRule = None
 
 
-#---------------------------------------------------------------------------
-# Here we define the keystroke rule.
-
-# This rule maps spoken-forms to actions.  Some of these 
-#  include special elements like the number with name "n" 
-#  or the dictation with name "text".  This rule is not 
-#  exported, but is referenced by other elements later on.
-#  It is derived from MappingRule, so that its "value" when 
-#  processing a recognition will be the right side of the 
-#  mapping: an action.
-# Note that this rule does not execute these actions, it
-#  simply returns them when it's value() method is called.
-#  For example "up 4" will give the value Key("up:4").
-#  More information about Key() actions can be found here:
-#  http://dragonfly.googlecode.com/svn/trunk/dragonfly/documentation/actionkey.html
-print("The real multiedit")
 class KeystrokeRule(MappingRule):
 
     exported = False
@@ -144,45 +69,18 @@ class KeystrokeRule(MappingRule):
     defaults = {
                 "n": 1,
                }
-    # Note: when processing a recognition, the *value* of 
-    #  this rule will be an action object from the right side 
-    #  of the mapping given above.  This is default behavior 
-    #  of the MappingRule class' value() method.  It also 
-    #  substitutes any "%(...)." within the action spec
-    #  with the appropriate spoken values.
 
 
-#---------------------------------------------------------------------------
-# Here we create an element which is the sequence of keystrokes.
 
-# First we create an element that references the keystroke rule.
-#  Note: when processing a recognition, the *value* of this element
-#  will be the value of the referenced rule: an action.
 alternatives = []
 alternatives.append(RuleRef(rule=KeystrokeRule()))
 if FormatRule:
     alternatives.append(RuleRef(rule=FormatRule()))
 single_action = Alternative(alternatives)
 
-# Second we create a repetition of keystroke elements.
-#  This element will match anywhere between 1 and 16 repetitions
-#  of the keystroke elements.  Note that we give this element
-#  the name "sequence" so that it can be used as an extra in
-#  the rule definition below.
-# Note: when processing a recognition, the *value* of this element
-#  will be a sequence of the contained elements: a sequence of
-#  actions.
 sequence = Repetition(single_action, min=1, max=16, name="sequence")
 
 
-#---------------------------------------------------------------------------
-# Here we define the top-level rule which the user can say.
-
-# This is the rule that actually handles recognitions. 
-#  When a recognition occurs, it's _process_recognition() 
-#  method will be called.  It receives information about the 
-#  recognition in the "extras" argument: the sequence of 
-#  actions and the number of times to repeat them.
 class RepeatRule(CompoundRule):
 
     # Here we define this rule's spoken-form and special elements.
@@ -195,12 +93,6 @@ class RepeatRule(CompoundRule):
                 "n": 1,                   # Default repeat count.
                }
 
-    # This method gets called when this rule is recognized.
-    # Arguments:
-    #  - node -- root node of the recognition parse tree.
-    #  - extras -- dict of the "extras" special elements:
-    #     . extras["sequence"] gives the sequence of actions.
-    #     . extras["n"] gives the repeat count.
     def _process_recognition(self, node, extras):
         sequence = extras["sequence"]   # A sequence of actions.
         count = extras["n"]             # An integer repeat count.
